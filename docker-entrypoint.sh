fe5fdcf797ef45355e48132d4fdcaae206f83eca
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-06-02 16:15:09 +0900

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# download fat-jar from s3
+aws --endpoint-url $ENDPOINT_URL s3 cp $APP_PATH $ASSEMBLY_NAME
+
 if [ "$1" = 'idle' ]; then
   exec tail -f /dev/null
 fi
@@ -13,8 +16,5 @@ if [ "$1" = 'worker' ]; then
   start-slave.sh spark://${MASTER_HOSTNAME:-master}:7077
   exec tail -f logs/$(ls logs)
 fi
-
-# download fat-jar from s3
-aws --endpoint-url $ENDPOINT_URL s3 cp $APP_PATH $ASSEMBLY_NAME
 
 exec "$@"
